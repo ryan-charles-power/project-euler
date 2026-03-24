fd09cdd5a228e33fb2d092fea5592dbea1ec6a35
Ryan Power <ryancharlespower@gmail.com>
2026-03-24 18:44:49 -0400

    completed problem3 in Go and Python

---
+++ problem3/go/solution.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num int64 = 600851475143
+	original := num
+	var divisor int64 = 2
+
+	// Loop until divisor squared exceeds the remaining number
+	for divisor^2 <= num {
+		// If the divisor is a factor
+		if num%divisor == 0 {
+			// Divide out all occurrences of this factor
+			for num%divisor == 0 {
+				num /= divisor
+			}
+		}
+		divisor++
+	}
+
+	// If num > 1, the remaining num is the largest prime factor
+	largest := num
+	if num == 1 {
+		largest = divisor - 1
+	}
+
+	fmt.Printf("The largest prime factor of %d is %d\n", original, largest)
+}
